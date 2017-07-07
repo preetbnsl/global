73f22315b1a91a640e0f9ab45e6b8991444f68fb
Gurpreet-256705 <dev.ios256705@gmail.com>
2017-07-08 00:11:37 +0530

    Add files via upload

---
+++ BorderObjects.swift
@@ -0,0 +1,131 @@
+//
+//  CustomBorderButton.swift
+//  BorderButton
+//
+//  Created by Rohit Pathak on 25/11/15.
+//  Copyright © 2015 Rohit Pathak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class BorderButton: UIButton  {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
+
+
+@IBDesignable
+class BorderLabel: UILabel  {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
+
+@IBDesignable
+class BorderImageView: UIImageView  {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
+
+@IBDesignable
+class BorderUIView: UIView  {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+}
+
+@IBDesignable
+class BorderUITextView: UITextView  {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+}
+
+
+@IBDesignable class BorderTextField: UITextField {
+    @IBInspectable var placeholderColor: UIColor = UIColor.white {
+        didSet {
+            if let placeholder = self.placeholder {
+                let attributes = [NSForegroundColorAttributeName: placeholderColor]
+                attributedPlaceholder = NSAttributedString(string: placeholder, attributes: attributes)
+            }
+        }
+    }
+}
+
